9ae13656091915c01f44f4697a3bd0626406979b
Diego Iberri R <creativoo@gmail.com>
2023-02-27 16:50:46 -0700

    Aprendiendo POO en distintos lenguajes de programacion

---
+++ Python/main.py
@@ -0,0 +1,13 @@
+from car import Car
+from account import Account
+
+if __name__ == "__main__":
+    print ("Hola Mundo")
+
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
+    print(vars(car))
+    print(vars(car.driver))
+
+    car2 = Car("QWE567", Account("Andrea Herrera", "ANDA123"))
+    print (vars(car2))
+    print(vars(car2.driver))
