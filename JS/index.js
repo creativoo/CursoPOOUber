b4d3bde9d2c3bdce248cea0ee08c6db0f267280d
Diego Iberri R <creativoo@gmail.com>
2023-02-27 18:19:30 -0700

    Agregando Herencias a Javascript y Python

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
 var car = new Car("AW456", new Account("Andres Herrera", "QIE687"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX ("AW456", new Account("Andrea Ferran", "ANDA765"), "Chevrolet", "Spark")
