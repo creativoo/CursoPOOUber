5950d7ebbf923792fb03d3f176eef95cd66fe952
Diego Iberri R <creativoo@gmail.com>
2023-02-27 19:04:59 -0700

    practicando encapsulamiento en JS

--- Java/main.java
+++ Java/main.java
@@ -1,12 +1,12 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
-        car.passenger = 4;
-        car.printDataCar();
+        UberX UberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Spark");
+        UberX.setPassenger(2);
+        UberX.printDataCar();
 
-        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "AMD456"));
-        car2.passenger = 3;
-        car2.printDataCar();
-        };
+//        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "AMD456"));
+//        car2.passenger = 3;
+//        car2.printDataCar();
+      };
     }
